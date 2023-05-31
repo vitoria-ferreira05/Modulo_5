d1c4d9d96161467554cd66130a78fef7059db394
vitoria.ferreira <vitoria.ferreira@zup.com.br>
2023-05-31 10:56:57 -0300

    nova alteração no exercicio 3

--- src/Exercicio3/Main.java
+++ src/Exercicio3/Main.java
@@ -7,24 +7,25 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+
         Map<String, List<String>> dicionario = new HashMap<>();
 
         List<String> bola = new ArrayList<>();
+        List<String> boneca = new ArrayList<>();
+        List<String> dado = new ArrayList<>();
+
         dicionario.put("bola", bola);
         bola.add("azul");
 
-        List<String> boneca = new ArrayList<>();
         dicionario.put("boneca", boneca);
         boneca.add("amarela");
 
-        List<String> dado = new ArrayList<>();
         dicionario.put("dado", dado);
         dado.add("rosa");
 
-
-        for (String chave : dicionario.keySet()){
-            List<String> valores = dicionario.get(chave);
-            System.out.println("A chave e: " + chave + " e os valores: " + valores);
+        for (String chaves : dicionario.keySet()){
+            List<String> valores = dicionario.get(chaves);
+            System.out.println("A chave e: " + chaves + " e os valores: " + valores);
         }
     }
 }
